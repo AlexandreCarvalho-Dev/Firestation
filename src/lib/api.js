b01b2535ee256ae1c9cac4ab5a76c60df39db70a
AlexandreCarvalho-Dev <alexdevprofissional@gmail.com>
2025-11-11 17:45:40 +0000

    Release Fixes 1.1

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,5 +1,5 @@
 // src/lib/api.js
-export const API_BASE = `http://${window.location.hostname}:3001`;
+export const API_BASE = import.meta.env.PROD ? '/api' : 'http://192.168.1.101:3001';
 
 async function asJson(res) {
   const data = await res.json().catch(() => ({}));
